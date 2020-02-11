0e6678454f241714353a00858a26e652afe621db
andrew <andrew7011616@gmail.com>
2020-02-11 10:53:49 -0800

    change web title

--- final-project/final-project/src/App.js
+++ final-project/final-project/src/App.js
@@ -6,6 +6,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>
+          Welcome to ENGL 282 cyber sercurity group website!
+        </h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
