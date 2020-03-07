b632674db07a4ecde00d62349176c7a6875bf9ce
andrew <andrew7011616@gmail.com>
2020-03-07 12:27:46 -0800

    format study content

--- final-project/final-project/src/components/studycase.js
+++ final-project/final-project/src/components/studycase.js
@@ -67,13 +67,23 @@ function Studycase(props) {
           </header>
           <div class="container">
           <h2>
-            Computers Will Destroy Us All
+            Computers Will Destroy Us All (copied)
           </h2>
           <h4>
             Written by Article Author. Published MM/DD/YYYY.
           </h4>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent eget risus vitae massa semper aliquam quis mattis quam. Morbi vitae tortor tempus, placerat leo et, suscipit lectus. Phasellus ut euismod massa, eu eleifend ipsum. Nulla eu neque commodo, dapibus dolor eget, dictum arcu. In nec purus eu tellus consequat ultricies. Donec feugiat tempor turpis, rutrum sagittis mi venenatis at. Sed molestie lorem a blandit congue. Ut pellentesque odio quis leo volutpat, vitae vulputate felis condimentum. Praesent vulputate fermentum lorem, id rhoncus sem vehicula eu. Quisque ullamcorper, orci adipiscing auctor viverra, velit arcu malesuada metus, in volutpat tellus sem at justo.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+            Praesent eget risus vitae massa semper aliquam quis mattis quam.
+             Morbi vitae tortor tempus, placerat leo et, suscipit lectus. 
+             Phasellus ut euismod massa, eu eleifend ipsum. Nulla eu neque 
+             commodo, dapibus dolor eget, dictum arcu. In nec purus eu tellus 
+             consequat ultricies. Donec feugiat tempor turpis, rutrum sagittis 
+             mi venenatis at. Sed molestie lorem a blandit congue. Ut pellentesque 
+             odio quis leo volutpat, vitae vulputate felis condimentum. Praesent
+              vulputate fermentum lorem, id rhoncus sem vehicula eu. Quisque 
+              ullamcorper, orci adipiscing auctor viverra, velit arcu malesuada 
+              metus, in volutpat tellus sem at justo.
           </p>
           <div class="mid-article"style={{margin:"5px 5px 200px"}}>
             <img src={img} alt="img"/>
@@ -82,10 +92,28 @@ function Studycase(props) {
             </p>
           </div>
           <p>
-            Fusce non fermentum mi. Praesent vel lobortis elit. Nulla sodales, risus quis sollicitudin iaculis, felis dolor aliquet purus, eget elementum velit nunc eu dolor. Curabitur elit tellus, dictum non sem sit amet, viverra lobortis velit. Quisque facilisis, neque cursus ullamcorper ornare, dolor metus aliquam lacus, pharetra porttitor massa neque ut ligula. Vivamus laoreet nulla diam, eget adipiscing felis scelerisque sit amet. Mauris egestas, nisi vitae cursus tincidunt, urna ipsum facilisis eros, ut venenatis dui tellus sit amet orci. Nullam a rhoncus diam, eget tristique felis. Nulla egestas adipiscing fermentum. Aenean sagittis libero at eros ornare molestie. Morbi convallis ornare imperdiet. Mauris convallis tristique erat ac eleifend.
+            Fusce non fermentum mi. Praesent vel lobortis elit. Nulla sodales, 
+            risus quis sollicitudin iaculis, felis dolor aliquet purus, eget 
+            elementum velit nunc eu dolor. Curabitur elit tellus, dictum non 
+            sem sit amet, viverra lobortis velit. Quisque facilisis, neque cursus
+            ullamcorper ornare, dolor metus aliquam lacus, pharetra porttitor
+            massa neque ut ligula. Vivamus laoreet nulla diam, eget adipiscing
+            felis scelerisque sit amet. Mauris egestas, nisi vitae cursus 
+            tincidunt, urna ipsum facilisis eros, ut venenatis dui tellus sit
+            amet orci. Nullam a rhoncus diam, eget tristique felis. Nulla egestas 
+            adipiscing fermentum. Aenean sagittis libero at eros ornare molestie. 
+            Morbi convallis ornare imperdiet. Mauris convallis tristique erat ac 
+            eleifend.
           </p>
           <p>
-            Nunc ullamcorper neque nunc, vitae sollicitudin nunc placerat nec. Suspendisse vel enim ultrices, fringilla urna luctus, lacinia est. Quisque mattis dictum sapien vitae laoreet. Suspendisse ultricies, <a href="">lorem</a> id tempor elementum, turpis magna pharetra purus, ac pulvinar metus ante quis nisi. Praesent dui massa, egestas dictum mi ut, tempus consequat neque. Maecenas urna tellus, fermentum at velit ac, commodo ultricies orci. Morbi augue dui, tincidunt et purus quis, consequat dictum enim.
+            Nunc ullamcorper neque nunc, vitae sollicitudin nunc placerat nec. 
+            Suspendisse vel enim ultrices, fringilla urna luctus, lacinia est. 
+            Quisque mattis dictum sapien vitae laoreet. Suspendisse ultricies, 
+            <a href="https://www.google.com">lorem</a> id tempor elementum, 
+            turpis magna pharetra purus, ac pulvinar metus ante quis nisi. 
+            Praesent dui massa, egestas dictum mi ut, tempus consequat neque. 
+            Maecenas urna tellus, fermentum at velit ac, commodo ultricies orci. 
+            Morbi augue dui, tincidunt et purus quis, consequat dictum enim.
           </p>
         </div>
         </TabPanel>
@@ -95,13 +123,23 @@ function Studycase(props) {
           </header>
           <div class="container">
           <h2>
-            We Will Destroy All Computers 
+            Us Will Destroy Computers All
           </h2>
           <h4>
             Written by Article Author. Published MM/DD/YYYY.
           </h4>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent eget risus vitae massa semper aliquam quis mattis quam. Morbi vitae tortor tempus, placerat leo et, suscipit lectus. Phasellus ut euismod massa, eu eleifend ipsum. Nulla eu neque commodo, dapibus dolor eget, dictum arcu. In nec purus eu tellus consequat ultricies. Donec feugiat tempor turpis, rutrum sagittis mi venenatis at. Sed molestie lorem a blandit congue. Ut pellentesque odio quis leo volutpat, vitae vulputate felis condimentum. Praesent vulputate fermentum lorem, id rhoncus sem vehicula eu. Quisque ullamcorper, orci adipiscing auctor viverra, velit arcu malesuada metus, in volutpat tellus sem at justo.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+            Praesent eget risus vitae massa semper aliquam quis mattis quam.
+             Morbi vitae tortor tempus, placerat leo et, suscipit lectus. 
+             Phasellus ut euismod massa, eu eleifend ipsum. Nulla eu neque 
+             commodo, dapibus dolor eget, dictum arcu. In nec purus eu tellus 
+             consequat ultricies. Donec feugiat tempor turpis, rutrum sagittis 
+             mi venenatis at. Sed molestie lorem a blandit congue. Ut pellentesque 
+             odio quis leo volutpat, vitae vulputate felis condimentum. Praesent
+              vulputate fermentum lorem, id rhoncus sem vehicula eu. Quisque 
+              ullamcorper, orci adipiscing auctor viverra, velit arcu malesuada 
+              metus, in volutpat tellus sem at justo.
           </p>
           <div class="mid-article"style={{margin:"5px 5px 200px"}}>
             <img src={img} alt="img"/>
@@ -110,10 +148,28 @@ function Studycase(props) {
             </p>
           </div>
           <p>
-            Fusce non fermentum mi. Praesent vel lobortis elit. Nulla sodales, risus quis sollicitudin iaculis, felis dolor aliquet purus, eget elementum velit nunc eu dolor. Curabitur elit tellus, dictum non sem sit amet, viverra lobortis velit. Quisque facilisis, neque cursus ullamcorper ornare, dolor metus aliquam lacus, pharetra porttitor massa neque ut ligula. Vivamus laoreet nulla diam, eget adipiscing felis scelerisque sit amet. Mauris egestas, nisi vitae cursus tincidunt, urna ipsum facilisis eros, ut venenatis dui tellus sit amet orci. Nullam a rhoncus diam, eget tristique felis. Nulla egestas adipiscing fermentum. Aenean sagittis libero at eros ornare molestie. Morbi convallis ornare imperdiet. Mauris convallis tristique erat ac eleifend.
+            Fusce non fermentum mi. Praesent vel lobortis elit. Nulla sodales, 
+            risus quis sollicitudin iaculis, felis dolor aliquet purus, eget 
+            elementum velit nunc eu dolor. Curabitur elit tellus, dictum non 
+            sem sit amet, viverra lobortis velit. Quisque facilisis, neque cursus
+            ullamcorper ornare, dolor metus aliquam lacus, pharetra porttitor
+            massa neque ut ligula. Vivamus laoreet nulla diam, eget adipiscing
+            felis scelerisque sit amet. Mauris egestas, nisi vitae cursus 
+            tincidunt, urna ipsum facilisis eros, ut venenatis dui tellus sit
+            amet orci. Nullam a rhoncus diam, eget tristique felis. Nulla egestas 
+            adipiscing fermentum. Aenean sagittis libero at eros ornare molestie. 
+            Morbi convallis ornare imperdiet. Mauris convallis tristique erat ac 
+            eleifend.
           </p>
           <p>
-            Nunc ullamcorper neque nunc, vitae sollicitudin nunc placerat nec. Suspendisse vel enim ultrices, fringilla urna luctus, lacinia est. Quisque mattis dictum sapien vitae laoreet. Suspendisse ultricies, <a href="">lorem</a> id tempor elementum, turpis magna pharetra purus, ac pulvinar metus ante quis nisi. Praesent dui massa, egestas dictum mi ut, tempus consequat neque. Maecenas urna tellus, fermentum at velit ac, commodo ultricies orci. Morbi augue dui, tincidunt et purus quis, consequat dictum enim.
+            Nunc ullamcorper neque nunc, vitae sollicitudin nunc placerat nec. 
+            Suspendisse vel enim ultrices, fringilla urna luctus, lacinia est. 
+            Quisque mattis dictum sapien vitae laoreet. Suspendisse ultricies, 
+            <a href="https://www.google.com">lorem</a> id tempor elementum, 
+            turpis magna pharetra purus, ac pulvinar metus ante quis nisi. 
+            Praesent dui massa, egestas dictum mi ut, tempus consequat neque. 
+            Maecenas urna tellus, fermentum at velit ac, commodo ultricies orci. 
+            Morbi augue dui, tincidunt et purus quis, consequat dictum enim.
           </p>
         </div>
         </TabPanel>
@@ -123,13 +179,23 @@ function Studycase(props) {
           </header>
           <div class="container">
           <h2>
-          Destroy Will Compute Us All
+            Destroy Will Compute Us All
           </h2>
           <h4>
             Written by Article Author. Published MM/DD/YYYY.
           </h4>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent eget risus vitae massa semper aliquam quis mattis quam. Morbi vitae tortor tempus, placerat leo et, suscipit lectus. Phasellus ut euismod massa, eu eleifend ipsum. Nulla eu neque commodo, dapibus dolor eget, dictum arcu. In nec purus eu tellus consequat ultricies. Donec feugiat tempor turpis, rutrum sagittis mi venenatis at. Sed molestie lorem a blandit congue. Ut pellentesque odio quis leo volutpat, vitae vulputate felis condimentum. Praesent vulputate fermentum lorem, id rhoncus sem vehicula eu. Quisque ullamcorper, orci adipiscing auctor viverra, velit arcu malesuada metus, in volutpat tellus sem at justo.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+            Praesent eget risus vitae massa semper aliquam quis mattis quam.
+             Morbi vitae tortor tempus, placerat leo et, suscipit lectus. 
+             Phasellus ut euismod massa, eu eleifend ipsum. Nulla eu neque 
+             commodo, dapibus dolor eget, dictum arcu. In nec purus eu tellus 
+             consequat ultricies. Donec feugiat tempor turpis, rutrum sagittis 
+             mi venenatis at. Sed molestie lorem a blandit congue. Ut pellentesque 
+             odio quis leo volutpat, vitae vulputate felis condimentum. Praesent
+              vulputate fermentum lorem, id rhoncus sem vehicula eu. Quisque 
+              ullamcorper, orci adipiscing auctor viverra, velit arcu malesuada 
+              metus, in volutpat tellus sem at justo.
           </p>
           <div class="mid-article"style={{margin:"5px 5px 200px"}}>
             <img src={img} alt="img"/>
@@ -138,10 +204,28 @@ function Studycase(props) {
             </p>
           </div>
           <p>
-            Fusce non fermentum mi. Praesent vel lobortis elit. Nulla sodales, risus quis sollicitudin iaculis, felis dolor aliquet purus, eget elementum velit nunc eu dolor. Curabitur elit tellus, dictum non sem sit amet, viverra lobortis velit. Quisque facilisis, neque cursus ullamcorper ornare, dolor metus aliquam lacus, pharetra porttitor massa neque ut ligula. Vivamus laoreet nulla diam, eget adipiscing felis scelerisque sit amet. Mauris egestas, nisi vitae cursus tincidunt, urna ipsum facilisis eros, ut venenatis dui tellus sit amet orci. Nullam a rhoncus diam, eget tristique felis. Nulla egestas adipiscing fermentum. Aenean sagittis libero at eros ornare molestie. Morbi convallis ornare imperdiet. Mauris convallis tristique erat ac eleifend.
+            Fusce non fermentum mi. Praesent vel lobortis elit. Nulla sodales, 
+            risus quis sollicitudin iaculis, felis dolor aliquet purus, eget 
+            elementum velit nunc eu dolor. Curabitur elit tellus, dictum non 
+            sem sit amet, viverra lobortis velit. Quisque facilisis, neque cursus
+            ullamcorper ornare, dolor metus aliquam lacus, pharetra porttitor
+            massa neque ut ligula. Vivamus laoreet nulla diam, eget adipiscing
+            felis scelerisque sit amet. Mauris egestas, nisi vitae cursus 
+            tincidunt, urna ipsum facilisis eros, ut venenatis dui tellus sit
+            amet orci. Nullam a rhoncus diam, eget tristique felis. Nulla egestas 
+            adipiscing fermentum. Aenean sagittis libero at eros ornare molestie. 
+            Morbi convallis ornare imperdiet. Mauris convallis tristique erat ac 
+            eleifend.
           </p>
           <p>
-            Nunc ullamcorper neque nunc, vitae sollicitudin nunc placerat nec. Suspendisse vel enim ultrices, fringilla urna luctus, lacinia est. Quisque mattis dictum sapien vitae laoreet. Suspendisse ultricies, <a href="">lorem</a> id tempor elementum, turpis magna pharetra purus, ac pulvinar metus ante quis nisi. Praesent dui massa, egestas dictum mi ut, tempus consequat neque. Maecenas urna tellus, fermentum at velit ac, commodo ultricies orci. Morbi augue dui, tincidunt et purus quis, consequat dictum enim.
+            Nunc ullamcorper neque nunc, vitae sollicitudin nunc placerat nec. 
+            Suspendisse vel enim ultrices, fringilla urna luctus, lacinia est. 
+            Quisque mattis dictum sapien vitae laoreet. Suspendisse ultricies, 
+            <a href="https://www.google.com">lorem</a> id tempor elementum, 
+            turpis magna pharetra purus, ac pulvinar metus ante quis nisi. 
+            Praesent dui massa, egestas dictum mi ut, tempus consequat neque. 
+            Maecenas urna tellus, fermentum at velit ac, commodo ultricies orci. 
+            Morbi augue dui, tincidunt et purus quis, consequat dictum enim.
           </p>
         </div>
         </TabPanel>
